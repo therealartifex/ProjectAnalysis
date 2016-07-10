339ffe0d84c0dabad6e0b854ce300ad1726ad273
Sceleris Artifex <linux.usaf@live.com>
2016-07-09 19:49:55 -0500

    input

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ProjectAnalysis
 {
@@ -7,6 +8,19 @@ namespace ProjectAnalysis
         private static void Main(string[] args)
         {
             Console.WriteLine(args);
+            var s = int.Parse(Console.ReadLine());
+            int[][] adjMat = new int[s][];
+
+            for (int i = 0; i < s; i++)
+            {
+                var stage = Console.ReadLine().Split(' ').Select(int.Parse);
+                var adj = stage.First();
+                var edges = stage.Skip(1).ToArray();
+                adjMat[i] = new int[s];
+
+                for (int j = 0; j < adj; j++) adjMat[i][0] = 0;
+            }
+
         }
     }
 }
